6eef6a9a523bc53ebea6b157eb30f6217d9ec28c
maxir143 <maxi.rodrigues.143@gmail.com>
2021-06-16 17:28:56 -0500

    working

--- main.py
+++ main.py
@@ -15,16 +15,15 @@ frameIcon.pack(side='top',fill='both')
 frameGamepad = Frame(window, height=150)
 frameGamepad.pack(side='bottom')
 
-
 # ------------------------------------------------------
 # --------------funciones-------------------------------
 # ------------------------------------------------------
 def resourcePath(relative_path):
-    try:  # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
+	try:  # PyInstaller creates a temp folder and stores path in _MEIPASS
+		base_path = sys._MEIPASS
+	except Exception:
+		base_path = os.path.abspath(".")
+	return os.path.join(base_path, relative_path)
 
 def windowTitleSet(text=''):
     window.title(text)
@@ -62,9 +61,9 @@ def gpButtonPress(btn, value=0, x=0, y=0):
     elif btn == 'HOME':
         gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_GUIDE)
     elif btn == 'RT':
-        gamepad.left_trigger_float(value)
-    elif btn == 'LT':
         gamepad.right_trigger_float(value)
+    elif btn == 'LT':
+        gamepad.left_trigger_float(value)
     elif btn == 'LJ':
         gamepad.left_joystick_float(x, y)
     elif btn == 'RJ':
@@ -111,20 +110,25 @@ def gpButtonRealease(btn):
         gamepad.right_joystick_float(0, 0)
     gamepad.update()
 def gpReset():
-    global gamepad
-    gamepad.reset()
-    gamepad.update()
+	if gpState():
+		global gamepad
+		gamepad.reset()
+		gamepad.update()
+def gpState():
+	if gamepad is not 0:
+		return 1
+	else:
+		windowTitleSet('control desconectado')
+		return 0
 
 #bonotes
 def btnCreate(name):
-	button_x = tk.Button(frameGamepad,text=name,width=5,height=2,compound = 'bottom', command= lambda:btnDriver(name))
+	button_x = tk.Button(frameGamepad,text=name,width=6,height=3,compound = 'bottom', command= lambda:btnDriver(name))
 	button_x.pack(side = LEFT)
 def btnDriver(name):
-	if gamepad is not 0:
+	if gpState():
 		gpButtonPress(name)
 		window.after(500, lambda: btnAlarm(name))
-	else:
-		windowTitleSet('control desconectado')
 def btnAlarm(name):
 	gpButtonRealease(name)
 
@@ -136,10 +140,12 @@ def emulatorConnect():
     label_control_status.configure(image=img_control_connected)  # cambiar icono de coneccion
     windowTitleSet('control conectado')
 def emulatorDisconnect():
-    global gamepad
-    gamepad = 0
-    label_control_status.configure(image=img_control_disconnected)  # cambiar icono de coneccion
-    camTestDriver()
+	global gamepad
+	gamepad = 0
+	label_control_status.configure(image=img_control_disconnected)  # cambiar icono de coneccion
+	camTestDriver()
+	accelReset()
+	dirReset()
 def emulatorDriver():
     if gamepad is 0:
         emulatorConnect()
@@ -187,20 +193,43 @@ def camTestAlarm(time=1000):
             gpButtonPress('RIGHT')
         window.after(time, lambda: camTestAlarm(time))
 
+
+def camChangeFront():
+	if gpState():
+		gpButtonPress('Y')
+		gpButtonPress('UP')
+		window.after(500, gpReset)
+
+def camChangeBack():
+	if gpState():
+		gpButtonPress('Y')
+		gpButtonPress('DOWN')
+		window.after(500, gpReset)
+
+
 # boton acelerador
 def accelerateDriver(value=0):
-    if gamepad is not 0:
-        gpButtonPress('RT', float(value/100))
-    else:
-        windowTitleSet('control desconectado')
+	if gpState() and value is not 0:
+		gpButtonPress('RT', float(value))
+		slider_accel.set(value)
+	else:
+		accelReset()
+def accelReset(accel=0):
+	slider_accel.set(0)
 
 # boton freno
 def stopDriver():
-    if gamepad is not 0:
-        gpButtonPress('RT', float(0))
-    else:
-        windowTitleSet('control desconectado')
+	accelerateDriver(0)
 
+#control direccion
+def dirDriver(dir=0):
+	if gpState():
+		gpButtonPress('LJ',x=float(dir),y=0)
+		slider_dir.set(dir)
+	else:
+		dirReset(0)
+def dirReset(dir=0):
+	slider_dir.set(0)
 
 # ------------------------------------------------------
 # --------Recursos--------------------------------------
@@ -214,7 +243,7 @@ img_control_disconnected = PhotoImage(file=resourcePath("disconnect.png")).subsa
 # ------------------------------------------------------
 # configuracion de la ventana
 window.resizable(False, False)
-window.title('Tortops')
+window.title('Emulador tortops')
 window.iconbitmap(resourcePath("favicon.ico"))
 
 # iconos
@@ -222,20 +251,39 @@ label_control_status = Label(frameIcon, image=img_control_disconnected)
 label_control_status.pack(side='right', fill='both')
 
 # Botones
-button_connect = tk.Button(frameIcon, text='CONECTAR EMULADOR', command=emulatorDriver)
+button_connect = tk.Button(frameIcon, text='CONECTAR EMULADOR',height=3, command=emulatorDriver)
 button_connect.pack(fill='both')
 
-button_cam_test = tk.Button(frameIcon, text='GIRAR CAMARA', command=camTestDriver)
-button_cam_test.pack(fill='both')
+#button_cam_test = tk.Button(frameIcon, text='GIRAR CAMARA',height=3, command=camTestDriver)
+#button_cam_test.pack(fill='both')
 
-button_accel = tk.Button(frameIcon, text='Acelerar', command= lambda :accelerateDriver(slider_accel.get()))
-button_accel.pack(fill='both')
+button_cam_change_front = tk.Button(frameIcon, text='CAMBIAR CAMARA FRONTAL',height=3, command=camChangeFront)
+button_cam_change_front.pack(fill='both')
 
-button_stop = tk.Button(frameIcon, text='Frenar', command= stopDriver)
-button_stop.pack(fill='both')
+button_cam_change_back = tk.Button(frameIcon, text='CAMBIAR CAMARA TRASERA',height=3, command=camChangeBack)
+button_cam_change_back.pack(fill='both')
 
-slider_accel = tk.Scale(frameIcon,from_=0,to=100,orient='horizontal')
+#button_stop = tk.Button(frameIcon, text='Frenar',height=3, command= stopDriver)
+#button_stop.pack(fill='both')
+
+slider_accel = tk.Scale(frameIcon,from_=0,to=1,resolution=.1,orient='horizontal',label='Velocidad',troughcolor='lightblue',command=accelerateDriver)
 slider_accel.pack(fill='both')
+slider_accel.bind('<ButtonRelease-1>', accelReset)
+
+slider_dir = tk.Scale(frameIcon,from_=-1,to=1,resolution=.1,orient='horizontal',label='Direccion',troughcolor='lightpink',command=dirDriver)
+slider_dir.pack(fill='both')
+slider_dir.bind('<ButtonRelease-1>', dirReset)
+
+#Control con teclado
+window.bind('<Up>',lambda x: accelerateDriver(1))
+window.bind('<Down>',lambda x: accelerateDriver(0))
+window.bind('<KeyRelease-Up>',lambda x: accelerateDriver(0))
+
+window.bind('<Left>',lambda x: dirDriver(-0.5))
+window.bind('<Right>',lambda x: dirDriver(.5))
+
+window.bind('<KeyRelease-Left>',lambda x: dirDriver(0))
+window.bind('<KeyRelease-Right>',lambda x: dirDriver(0))
 
 # game pad
 
